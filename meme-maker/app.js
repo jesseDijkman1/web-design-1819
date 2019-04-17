7b67cd97d7ea5e02eff1af279f15fe7d61b36ffc
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-04-17 10:26:31 +0200

    Added ejs and express-session

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -1,11 +1,22 @@
-const express = require("express");
+const express = require("express"),
+      session = require("express-session"),
+      ejs = require("ejs");
 
 const port = 3000;
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 app.use(express.static("public"));
+app.use(session({secret: "Shh, its a secret!"}));
 
-app.get("/", (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+app.get("/", (req, res) => {
+  const data = {
+    sessionID: req.session.id
+  }
+
+  res.render("index.ejs", data)
+});
 
 app.listen(port, () => console.log(`Listening to port: ${port}`));
