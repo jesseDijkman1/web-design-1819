b1d3e328df4cd16d876f1ec562e2b6e3ae413feb
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-04-17 11:00:06 +0200

    Added the first step page (file upload)

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -2,7 +2,9 @@ const express = require("express"),
       session = require("express-session"),
       ejs = require("ejs");
 
-const port = 3000;
+const port = 3000,
+      users = {},
+      memes = {}
 
 const app = express();
 
@@ -11,12 +13,60 @@ app.set('views', 'views');
 app.use(express.static("public"));
 app.use(session({secret: "Shh, its a secret!"}));
 
-app.get("/", (req, res) => {
-  const data = {
-    sessionID: req.session.id
+class Meme {
+  constructor(data) {
+    this.userID = data.userID;
+    this.step = data.step;
+  }
+}
+
+class User {
+  constructor(data) {
+    this.sessionID = data.sessionID
+    this.currentStep;
   }
 
-  res.render("index.ejs", data)
+  updateStep(step) {
+    this.currentStep = step;
+
+    if (!memes[this.sessionID]) {
+      memes[this.sessionID] = new Meme({
+        userID: this.sessionID,
+        step: this.currentStep
+      })
+    } else {
+      memes[this.sessionID].step = this.currentStep
+    }
+  }
+}
+
+
+
+app.get("/", (req, res) => {
+  if (!users[req.session.id]) {
+    users[req.session.id] = new User({
+      sessionID: req.session.id
+    })
+  }
+
+  res.render("index.ejs", {
+    sessionID: req.session.id
+  })
 });
+
+app.get("/meme-maker", (req, res) => {
+  const step = req.query.s,
+        sessionID = req.query.id;
+
+  if (!sessionID || !step) {
+    return res.redirect("/");
+  }
+
+  users[sessionID].updateStep(step);
+
+  res.render("imageUpload.ejs", {
+    sessionID: sessionID
+  })
+})
 
 app.listen(port, () => console.log(`Listening to port: ${port}`));
